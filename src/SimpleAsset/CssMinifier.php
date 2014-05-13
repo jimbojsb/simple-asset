c0d3f20261abf527e436f320b43727ab20c3c5f3
Josh Butts <josh@joshbutts.com>
2014-05-12 22:01:11 -0500

    remove irrelevant error handling in css minifier

--- src/SimpleAsset/CssMinifier.php
+++ src/SimpleAsset/CssMinifier.php
@@ -5,10 +5,6 @@ class CssMinifier
 {
     public function minify($sourceFile, $destinationFile)
     {
-        if (!is_writable(dirname($destinationFile))) {
-            throw new \Exception('Unable to create/write to ' . dirname($destinationFile));
-        }
-
         $cmd = sprintf("/usr/bin/env csso --input %s --ouptut %s", $sourceFile, $destinationFile);
         system($cmd);
     }
